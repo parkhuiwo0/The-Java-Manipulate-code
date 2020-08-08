1908c1b4a70b0d07e1cf9e40619e95c28bb2c597
HuiwooPark <parkhuiwo0@gmail.com>
2020-08-08 17:32:09 +0900

    모자에서 토끼를 꺼내는 방법 2

--- src/main/java/Masulsa.java
+++ src/main/java/Masulsa.java
@@ -1,6 +1,8 @@
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
 import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,16 +16,28 @@ import java.io.IOException;
 public class Masulsa {
 
     public static void main(String[] args) {
+//        ClassLoader classLoader = Masulsa.class.getClassLoader();
+//        TypePool typePool = TypePool.Default.of(classLoader);
+//
+//        try {
+//            new ByteBuddy().redefine(
+//                    typePool.describe("Moja").resolve(),
+//                    ClassFileLocator.ForClassLoader.of(classLoader))
+//                    .method(ElementMatchers.named("pullOut")).intercept(FixedValue.value("Rabbit !"))
+//                    .make().saveIn(new File("/Users/common/Desktop/study-project/build/classes/"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-        try {
-            new ByteBuddy().redefine(Moja.class)
-                    .method(ElementMatchers.named("pullOut")).intercept(FixedValue.value("Rabbit !"))
-                    .make().saveIn(new File("/Users/common/Desktop/study-project/build/classes/"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            new ByteBuddy().redefine(Moja.class)
+//                    .method(ElementMatchers.named("pullOut")).intercept(FixedValue.value("Rabbit !"))
+//                    .make().saveIn(new File("/Users/common/Desktop/study-project/build/classes/"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-//        System.out.println(new Moja().pullOut());
+        System.out.println(new Moja().pullOut());
 
 
     }
